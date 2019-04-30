226e7b8c85892e3b1dc86a397e5ea382f00fde54
TibiG97 <tiberiucopaciu@gmail.com>
2019-05-01 00:09:58 +0100

    add core functions and parameters of MLP

--- ML_Module/mlp.py
+++ ML_Module/mlp.py
@@ -6,17 +6,16 @@ import shutil
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-
-from keras.preprocessing.text import Tokenizer
 from sklearn.preprocessing import LabelBinarizer
 
+from keras.preprocessing.text import Tokenizer
+from keras.wrappers.scikit_learn import KerasClassifier
 from keras.models import Sequential
+from keras.layers import Activation
 from keras.layers import Dense
-from keras.layers import Activation, Dense, Dropout
+from keras.layers import Dropout
+from keras.layers import BatchNormalization
 from keras.optimizers import Adam
-
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
 
 class MultilayerPerceptron(NeuralNetwork):
@@ -26,40 +25,66 @@ class MultilayerPerceptron(NeuralNetwork):
     """
 
     def __init__(self,
-                 hidden_size,
-                 num_classes,
-                 dropout,
-                 input_size,
-                 criterion,
-                 optimiser,
-                 batch_size,
-                 num_epochs):
-        self.name = 'MLP'
+                 batch_size: int,
+                 epochs: int,
+                 learning_rate: float,
+                 dropout_rate: float,
+                 init_mode: str,
+                 no_of_classes: int,
+                 verbose: int,
+                 hidden_size: int,
+                 input_size: int):
+
         self.hidden_size = hidden_size
-        self.num_classes = num_classes
-        self.dropout = dropout
         self.input_size = input_size
-        self.criterion = criterion
-        self.optimiser = optimiser
-        self.batch_size = batch_size
-        self.num_epochs = num_epochs
-        self.mlp = self.__create_model()
-        self.history = None
+
+        self.model = KerasClassifier(build_fn=self.__create_model,
+                                     epochs=epochs,
+                                     batch_size=batch_size,
+                                     verbose=verbose)
+
+        super(MultilayerPerceptron, self).__init__(
+            classifier=self.classifier,
+            process_data=self.process_data,
+            batch_size=batch_size,
+            epochs=epochs,
+            learning_rate=learning_rate,
+            dropout_rate=dropout_rate,
+            init_mode=init_mode,
+            no_of_classes=no_of_classes,
+            verbose=verbose,
+            name='MLP'
+        )
 
     def __create_model(self):
-        nn = Sequential()
-        nn.add(Dense(self.hidden_size, input_shape=(self.input_size,)))
-        nn.add(Activation('relu'))
-        nn.add(Dropout(self.dropout))
-        nn.add(Dense(self.num_classes))
-        nn.add(Activation('softmax'))
-        nn.summary()
+        model = Sequential()
+        model.add(Dense(self.hidden_size, input_shape=(self.input_size,)))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Dropout(self.dropout_rate))
 
-        return nn
+        if self.no_of_classes > 2:
+            model.add(Dense(self.no_of_classes,
+                            activation='softmax'))
+
+            model.compile(loss="categorical_crossentropy",
+                          optimizer=Adam(self.learning_rate),
+                          metrics=["accuracy"])
+
+        else:
+            model.add(Dense(1,
+                            activation="sigmoid"))
+
+            model.compile(loss="binary_crossentropy",
+                          optimizer=Adam(self.learning_rate),
+                          metrics=["accuracy"])
+
+        return model
 
     def __process_data(self):
         return
 
+    @staticmethod
     def copy_data(self, src_file_path, dst_file_path):
         if not os.path.exists(dst_file_path):
             os.mkdir(dst_file_path)
@@ -68,6 +93,7 @@ class MultilayerPerceptron(NeuralNetwork):
                 logfile_name = logfile.split('/')[-1]
                 shutil.copyfile(logfile, dst_file_path + "/" + logfile_name)
 
+    @staticmethod
     def read_data(self, logfile_path):
         log_collection = pd.DataFrame()
         logs = pd.DataFrame()
@@ -84,16 +110,3 @@ class MultilayerPerceptron(NeuralNetwork):
         log_collection = log_collection.reset_index(drop=True)
 
         return log_collection
-
-    def train(self,
-              X_train,
-              y_train):
-        self.mlp.compile(loss=self.criterion,
-                         optimizer=self.optimiser,
-                         metrics=['accuracy'])
-
-        self.history = self.mlp.fit(X_train, y_train,
-                                    batch_size=self.batch_size,
-                                    epochs=self.num_epochs,
-                                    verbose=1,
-                                    validation_split=0.1)
